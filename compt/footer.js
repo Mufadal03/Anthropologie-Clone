59b3cc38e33b01c08ea5c06e47f47efdb0258ab2
Yogesh Jagtap <yjagtap667@gmail.com>
2022-06-17 02:31:46 +0530

    added home page

---
+++ compt/footer.js
@@ -0,0 +1,172 @@
+function footer() {
+  return `
+  <footer>
+  <hr  class="color"/>
+  <div id="New_arrival">
+    <div id="footer_main" class="footer_main">
+      <div id="signin">
+        <h2>Sign Up for Email</h2>
+        <p>
+          Receive early access to new arrivals, sales, exclusive content,
+          events and much more!
+        </p>
+      </div>
+      <div id="mail">
+        <label for="">Email Address*</label><br>
+        <input type="email" placeholder="" class="Input" id="input_main"/>
+        <input type="submit" class="Submit" value="SUBMIT" id="submit_main"/>
+      </div>
+    </div>
+    <p class="read">
+      By signing up, you will receive Anthropologie offers, promotions and
+      other commercial messages. You are also agreeing to Anthropologie’s <a href="">Privacy Policy</a>. You may unsubscribe at any time.
+    </p>
+  </div>
+  <hr  class="color"/>
+  <div id="footer2">
+    <div class="ft-ha">
+      <p>Help</p>
+      <ul>
+        <!-- <li><a href="">Home</a></li> -->
+        <li><a href="https://www.anthropologie.com/?ref=logo#order-inquiry">Track Your Order</a></li>
+        <li><a href="https://www.anthropologie.com/?ref=logo#order-inquiry">Start A Return Or Exchangr(US)</a></li>
+        <li><a href="https://www.anthropologie.com/help/returns-exchanges">Return & Exchanges</a></li>
+        <li><a href="https://www.anthropologie.com/help/shipping-info">Shipping</a></li>
+        <li><a href="https://www.anthropologie.com/help/customer-service">Customer Srevice</a></li>
+        <li><a href="https://www.anthropologie.com/help/current-promotions">Curent Promotions</a></li>
+        <li><a href="https://www.anthropologie.com/help/recall">Product Recalls</a></li>
+      </ul>
+    </div>
+
+    <div class="ft-ha">
+      <p>About</p>
+      <ul>
+        
+        <li><a href="https://www.anthropologie.com/help/our-story">Our Story</a></li>
+        <li><a href="https://anthropologieevents.splashthat.com/">Events</a></li>
+        <li><a href="https://www.anthropologie.com/help/stories-community-greater-good">A Great Good</a></li>
+        <li><a href="https://www.anthropologie.com/help/diversity-and-inclusion">Diversity & Inclusion</a></li>
+        <li><a href="https://www.anthropologie.com/stories">Stories</a></li>
+      </ul>
+    </div>
+    <div class="ft-ha">
+      <p>Services</p>
+      <ul>
+        
+        <li><a href="https://www.anthropologie.com/help/anthro-loyalty">Anthor Perks</a></li>
+        <li><a href="https://www.anthropologie.com/help/gift-card">Gift Carts</a></li>
+        <li><a href="https://www.anthropologie.com/help/design-trade-program">Trade Membership</a></li>
+        <li><a href="https://www.anthropologie.com/help/furniture-guide">Furniture: Guides & services</a></li>
+        <li><a href="https://www.anthropologie.com/help/curbside-pick-up">Store Pickup & Collection Pionts</a></li>
+        <li><a href="https://www.anthropologie.com/help/klarna">Klarna</a></li>
+      </ul>
+    </div>
+
+    <div class="ft-ha">
+      <p>Connect</p>
+      <ul>
+        
+        <li><a href="https://www.anthropologie.com/help/contact-us">Contact Us</a></li>
+        <li><a href="https://www.anthropologie.com/help/connect-with-us">Stay Connected</a></li>
+        <li><a href="https://www.urbn.com/work-with-us">Careers</a></li>
+        <li><a href="https://www.anthropologie.com/help/personal-styling">Styling Services</a></li>
+        <li><a href="https://www.anthropologie.com/help/request-catalog">Request A Catalog</a></li>
+      </ul>
+    </div>
+    <div class="lo-st">
+      <div>
+        <a href="">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/67/67347.png"
+            alt=""
+            id="images_icon"
+          />
+          Store location</a
+        >
+      </div>
+      <div>
+        <a href=""
+          ><img
+            src="http://assets.stickpng.com/images/584856b4e0bb315b0f7675ac.png"
+            alt=""
+            id="images_icon1"
+          />
+          Get Email</a
+        >
+      </div>
+    </div>
+  </div>
+  <div id="icon">
+    <a href="https://itunes.apple.com/us/app/anthropologie/id922496703?mt=8"
+      ><img
+        src="https://images.ctfassets.net/5de70he6op10/4gWq1G4qLLgmqlNeHfzV2T/55298745127c36c5876f4013434c0c37/Download_on_the_App_Store_Badge_US-UK_RGB_wht_092917_Copy_2.svg"
+        alt=""
+        class="app_store"
+    /></a>
+
+    <div>
+      <a href="https://in.pinterest.com/ideas/"><img
+        src="https://i.pinimg.com/originals/fd/24/fe/fd24fe4adfd1e7d0ca36bc476b6b5005.png"
+        alt=""
+        class="social"
+      /></a>
+      <a href="https://www.instagram.com/anthropologie/"><img
+        src="https://cdn-icons-png.flaticon.com/512/1077/1077042.png"
+        alt=""
+        class="social"
+      /></a>
+
+      <a href="https://www.facebook.com/Anthropologie"><img
+        src="http://davidpelandcompany.com/wp-content/uploads/2020/12/facebook-social-media-fb-logo-square.png"
+        alt=""
+        class="social"
+      /></a>
+
+      <a href="https://twitter.com/anthropologie"><img
+        src="https://cdn-icons-png.flaticon.com/512/1532/1532475.png"
+        alt=""
+        class="social"
+      /></a>
+    </div>
+  </div>
+  <div id="sail_image">
+    <img
+      src="https://images.ctfassets.net/5de70he6op10/2DRgCRXNIEkk0wIqUo2ywa/20ea44a95fb361eaf034b42eec179d44/_site_footer__download_app_enable_push_.jpg"
+      alt=""
+    />
+  </div>
+  <div id="country">
+    <p><a href="">US</a></p>
+    <hr />
+    <p><a href="">France</a></p>
+    <hr />
+    <p><a href="">Germany</a></p>
+    <hr />
+    <p><a href="">Italy</a></p>
+    <hr />
+    <p><a href="">Spain</a></p>
+    <hr />
+    <p><a href="">UK</a></p>
+  </div>
+  <div id="bottom_line">
+    <div id="terms">
+      <p><a href="">Privacy Policy </a></p>
+      <hr />
+      <p><a href="">Terms of Use</a></p>
+      <hr />
+      <p><a href="">CA Transparency</a></p>
+      <hr />
+      <p><a href="">Accessibility</a></p>
+      <hr />
+      <p><a href="">URBN.com</a></p>
+      <hr />
+      <p>For CA Residents</p>
+    </div>
+    <div><p>2022 URBN.com All Rights Reserved</p></div>
+  </div>
+  </footer>
+    `;
+}
+
+
+export default footer;
